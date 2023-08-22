0e7978c45eda70616e38b02bb0eb742b2b3904c3
987456987 <donufrak112697@gmail.com>
2023-08-22 16:30:14 -0700

    custom tooltip

--- csgo-stats/src/renderer/src/components/UserStats.jsx
+++ csgo-stats/src/renderer/src/components/UserStats.jsx
@@ -2,16 +2,32 @@ import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip } from 'recharts'
 
 function UserStats({ data, userId }) {
   const adrData = data.map((match) => ({
-    ADR: match.match_stats.find((player) => player.player_id === userId).adr
+    ADR: match.match_stats.find((player) => player.player_id === userId).adr,
+    date: convertUnixToYMD(match.match_info.finished)
   }));
+
+  function CustomTooltip({ active, payload, label }) {
+    if (active && payload && payload.length) {
+      const dataPoint = payload[0].payload;
+      return (
+        <div className="custom-tooltip">
+          <p>{`Date: ${dataPoint.date}`}</p>
+          <p>{`ADR: ${dataPoint.ADR}`}</p>
+        </div>
+      );
+    }
+
+    return null;
+  }
 
   function renderChart() {
     return (
       <LineChart width={600} height={300} data={adrData}>
         <Line type="monotone" dataKey="ADR" stroke="#8884d8" />
         <CartesianGrid stroke="#ccc" />
-        <YAxis />
-        <Tooltip label={"ADR"}/>
+        <XAxis dataKey="date" />
+        <YAxis tickFormatter={(value) => convertUnixToYMD(value)} />
+        <Tooltip content={<CustomTooltip />} />
       </LineChart>
     );
   }
@@ -20,15 +36,15 @@ function UserStats({ data, userId }) {
 }
 
 function convertUnixToYMD(unixTime) {
-  const date = new Date(unixTime * 1000) // Convert Unix time to milliseconds
-  const year = date.getFullYear()
-  const month = date.getMonth() + 1 // Months are zero-based, so add 1
-  const day = date.getDate()
+  const date = new Date(unixTime * 1000);
+  const year = date.getFullYear();
+  const month = date.getMonth() + 1;
+  const day = date.getDate();
 
   const formattedDate = `${year}-${month.toString().padStart(2, '0')}-${day
     .toString()
-    .padStart(2, '0')}`
-  return formattedDate
+    .padStart(2, '0')}`;
+  return formattedDate;
 }
 
 export default UserStats;

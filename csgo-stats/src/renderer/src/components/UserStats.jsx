fbab4c33ff4d52da8e903ffdf1de1f711ddbe04a
987456987 <donufrak112697@gmail.com>
2023-08-23 11:00:00 -0700

    Fix

--- csgo-stats/src/renderer/src/components/UserStats.jsx
+++ csgo-stats/src/renderer/src/components/UserStats.jsx
@@ -1,50 +1,56 @@
-import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip } from 'recharts';
+import { AreaChart, Area, CartesianGrid, XAxis, YAxis, Tooltip } from 'recharts'
 
 function UserStats({ data, userId }) {
   const adrData = data.map((match) => ({
     ADR: match.match_stats.find((player) => player.player_id === userId).adr,
     date: convertUnixToYMD(match.match_info.finished)
-  }));
+  }))
 
-  function CustomTooltip({ active, payload, label }) {
+  function CustomTooltip({ active, payload }) {
     if (active && payload && payload.length) {
-      const dataPoint = payload[0].payload;
+      const dataPoint = payload[0].payload
       return (
         <div className="custom-tooltip">
           <p>{`Date: ${dataPoint.date}`}</p>
           <p>{`ADR: ${dataPoint.ADR}`}</p>
         </div>
-      );
+      )
     }
-
-    return null;
+    return null
   }
 
   function renderChart() {
     return (
-      <LineChart width={600} height={300} data={adrData}>
-        <Line type="monotone" dataKey="ADR" stroke="#8884d8" />
-        <CartesianGrid stroke="#ccc" />
-        <XAxis dataKey="date" />
-        <YAxis tickFormatter={(value) => convertUnixToYMD(value)} />
-        <Tooltip content={<CustomTooltip />} />
-      </LineChart>
-    );
+      <div className="chart-container">
+        <AreaChart width={600} height={300} data={adrData}>
+          <Area type="monotone" dataKey="ADR" stroke="#7096b3" fill="#7096b3" />
+          <CartesianGrid stroke="gray" strokeDasharray="3 3" />
+          <XAxis dataKey="date" hide={true} />
+          <YAxis
+            domain={[
+              Math.min(...adrData.map((entry) => entry.ADR)) - 10,
+              Math.max(...adrData.map((entry) => entry.ADR)) + 10
+            ]}
+          />
+          <Tooltip content={<CustomTooltip />} />
+        </AreaChart>
+      </div>
+    )
   }
 
-  return <>{renderChart()}</>;
+  return <>{renderChart()}</>
 }
 
 function convertUnixToYMD(unixTime) {
-  const date = new Date(unixTime * 1000);
-  const year = date.getFullYear();
-  const month = date.getMonth() + 1;
-  const day = date.getDate();
+  const date = new Date(unixTime * 1000)
+  const year = date.getFullYear()
+  const month = date.getMonth() + 1
+  const day = date.getDate()
 
   const formattedDate = `${year}-${month.toString().padStart(2, '0')}-${day
     .toString()
-    .padStart(2, '0')}`;
-  return formattedDate;
+    .padStart(2, '0')}`
+  return formattedDate
 }
 
-export default UserStats;
+export default UserStats

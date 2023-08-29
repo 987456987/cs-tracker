d6fb5a5d322d7801f7d2b2e05fb3631fd4261cdb
987456987 <donufrak112697@gmail.com>
2023-08-28 22:17:54 -0700

    fixed missing data issue

--- csgo-stats/src/renderer/src/components/Match.jsx
+++ csgo-stats/src/renderer/src/components/Match.jsx
@@ -6,21 +6,7 @@ function Match({ data }) {
 
   const currentMatch = data.find((match) => match.match_info.match_id === matchId)
 
-  const playerStats = currentMatch.match_stats
-
-  const teamA = []
-  const teamB = []
-  playerStats.forEach((player) => {
-    if (player.team_id == currentMatch.team_info[0].team_id) {
-      teamA.push(player)
-    }
-    if (player.team_id == currentMatch.team_info[1].team_id) {
-      teamB.push(player)
-    }
-  })
-
   console.log(currentMatch)
-  if (currentMatch) console.log(currentMatch.match_stats)
   return currentMatch ? (
     <>
       <div className="match-list">
@@ -36,26 +22,50 @@ function Match({ data }) {
             </tr>
           </thead>
           <tbody>
-            {teamA.map((player) => (
-              <tr key={player.player_id}>
-                <td>{player.nickname}</td>
-                <td>{player.kills}</td>
-                <td>{player.assists}</td>
-                <td>{player.deaths}</td>
-                <td>{player.adr}</td>
-                <td>{player.counter_strafing}</td>
-              </tr>
-            ))}
-            {teamB.map((player) => (
-              <tr key={player.player_id}>
-                <td>{player.nickname}</td>
-                <td>{player.kills}</td>
-                <td>{player.assists}</td>
-                <td>{player.deaths}</td>
-                <td>{player.adr}</td>
-                <td>{player.counter_strafing}</td>
-              </tr>
-            ))}
+            {currentMatch.match_stats.map((player) => {
+              if (player.team_id === currentMatch.team_info[0].team_id) {
+                return (
+                  <tr key={player.player_id}>
+                    <td>{player.nickname}</td>
+                    <td>{player.kills}</td>
+                    <td>{player.assists}</td>
+                    <td>{player.deaths}</td>
+                    <td>{player.adr}</td>
+                    <td>{player.counter_strafing}</td>
+                  </tr>
+                )
+              }
+              return null // Add this line if you want to skip rendering for other teams
+            })}
+          </tbody>
+        </table>
+        <table>
+          <thead>
+            <tr>
+              <th>Player</th>
+              <th>Kills</th>
+              <th>Assists</th>
+              <th>Deaths</th>
+              <th>ADR</th>
+              <th>Counter Strafe</th>
+            </tr>
+          </thead>
+          <tbody>
+            {currentMatch.match_stats.map((player) => {
+              if (player.team_id === currentMatch.team_info[1].team_id) {
+                return (
+                  <tr key={player.player_id}>
+                    <td>{player.nickname}</td>
+                    <td>{player.kills}</td>
+                    <td>{player.assists}</td>
+                    <td>{player.deaths}</td>
+                    <td>{player.adr}</td>
+                    <td>{player.counter_strafing}</td>
+                  </tr>
+                )
+              }
+              return null // Add this line if you want to skip rendering for other teams
+            })}
           </tbody>
         </table>
       </div>

004eecb17aaba548c295790777d28a9fee027cc3
987456987 <donufrak112697@gmail.com>
2023-08-28 21:37:47 -0700

    split table by team

--- csgo-stats/src/renderer/src/components/Match.jsx
+++ csgo-stats/src/renderer/src/components/Match.jsx
@@ -6,6 +6,19 @@ function Match({ data }) {
 
   const currentMatch = data.find((match) => match.match_info.match_id === matchId)
 
+  const playerStats = currentMatch.match_stats
+
+  const teamA = []
+  const teamB = []
+  playerStats.forEach((player) => {
+    if (player.team_id == currentMatch.team_info[0].team_id) {
+      teamA.push(player)
+    }
+    if (player.team_id == currentMatch.team_info[1].team_id) {
+      teamB.push(player)
+    }
+  })
+
   console.log(currentMatch)
   if (currentMatch) console.log(currentMatch.match_stats)
   return currentMatch ? (
@@ -23,7 +36,17 @@ function Match({ data }) {
             </tr>
           </thead>
           <tbody>
-            {currentMatch.match_stats.map((player) => (
+            {teamA.map((player) => (
+              <tr key={player.player_id}>
+                <td>{player.nickname}</td>
+                <td>{player.kills}</td>
+                <td>{player.assists}</td>
+                <td>{player.deaths}</td>
+                <td>{player.adr}</td>
+                <td>{player.counter_strafing}</td>
+              </tr>
+            ))}
+            {teamB.map((player) => (
               <tr key={player.player_id}>
                 <td>{player.nickname}</td>
                 <td>{player.kills}</td>
